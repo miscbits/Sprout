61bf146c611c9bfd5761392939fa775314d30911
miscbits <willow6786@gmail.com>
2018-12-19 14:03:39 -0500

    added navbar vue and bootstrap

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    title: String,
+    logo: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
